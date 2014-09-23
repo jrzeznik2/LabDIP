5cf7cb0788142f47f64d00c0684f472597fb4d65
Jacob <Jacob@10.30.19.218>
2014-09-23 14:34:18 -0500

    Final Commit for due date

--- src/dip/lab2/ServiceTipCalculator.java
+++ src/dip/lab2/ServiceTipCalculator.java
@@ -10,5 +10,14 @@ package dip.lab2;
  * @author Jacob
  */
 public interface ServiceTipCalculator {
+    public static final double MIN_BILL = 0.00;
+    public static final String BILL_ENTRY_ERR =
+            "Error: bill must be greater than or equal to " + MIN_BILL;
+    public static final double GOOD_RATE = 0.20;
+    public static final double FAIR_RATE = 0.15;
+    public static final double POOR_RATE = 0.10;
     public double getTip();
+    public static enum ServiceQuality {
+        GOOD, FAIR, POOR
+    }
 }
